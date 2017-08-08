31ecb9074b35fcc6967520c45cc0394f40f72754
dkniazevych <dkniazevych@hiqinvest.nl>
2017-08-08 12:25:05 +0200

    remove lib/ from Git

--- lib/fonts.d.ts
+++
@@ -1,4 +0,0 @@
-import { Archive } from 'file2html-archive-tools';
-import { Relations } from './index';
-export declare const folderName: string;
-export declare function parseFonts(picturesFolder: Archive): Promise<Relations>;
--- lib/parse-css.d.ts
+++
@@ -1,3 +0,0 @@
-import { ArchiveEntry } from 'file2html-archive-tools';
-import { Relations } from './index';
-export default function parseCSS(file: ArchiveEntry, relations: Relations): Promise<string>;
